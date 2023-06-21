126c61c911d796645f678f441f9b348c77bf23c1
itsregalo <itsregalo047@gmail.com>
2023-06-21 06:53:49 +0300

    fixed git issues

---
+++ frontend/accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    
+    class Meta:
+        db_table = 'user'
